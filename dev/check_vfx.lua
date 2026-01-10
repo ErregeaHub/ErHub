c7329399b01c9779b4612159c060162c2283acd7
Garda <Garda@MYMAC.local>
2026-01-10 18:47:41 +0700

    cleanup lite branch

--- dev/check_vfx.lua
+++
@@ -1,6 +0,0 @@
-local vfx = game:GetService("ReplicatedStorage"):FindFirstChild("Modules") and game:GetService("ReplicatedStorage").Modules:FindFirstChild("VFX")
-if vfx then
-    print("VFX found: " .. vfx.ClassName)
-else
-    print("VFX not found")
-end
